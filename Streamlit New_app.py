acf46bd109345cb3c15da175c6463ba14f69b25e
K-Anme <lutfhiasalwanif22@gmail.com>
2025-07-12 09:08:51 +0700

    Streamlit New_app.py

--- Streamlit New_app.py
+++ Streamlit New_app.py
@@ -4,3 +4,13 @@ st.write("Hello, *World!* :sunglasses:")
 import streamlit as st
 
 st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+import streamlit as st
+
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
+if st.button("Aloha", type="tertiary"):
+    st.write("Ciao")
